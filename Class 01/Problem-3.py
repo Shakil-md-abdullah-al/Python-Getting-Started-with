b5e09e239666262ca20ad161ec73668eaca41126
MD Abdullah AL Shakil <shakil3300bd@gmail.com>
2024-09-03 11:04:28 +0600

    Problem-3:pyttsx3

---
+++ Class 01/Problem-3.py
@@ -0,0 +1,7 @@
+# Works with an external module 
+# pyttx3--> Python Text to Speech
+# Copy the code and paste
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I an Shakil")
+engine.runAndWait()
